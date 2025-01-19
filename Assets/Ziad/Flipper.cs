ce4c22361541b848851759a08fac502de055e25b
Ziad El-Rady <thatoneguy2439@gmail.com>
2025-01-19 05:02:20 -0500

    Flipper fixed, no longer adds force when in normal spot

--- Assets/Ziad/Flipper.cs
+++ Assets/Ziad/Flipper.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Flipper : MonoBehaviour
 {
     public float Force;
+    public Transform Pivot;
+
+    private void Start() {
+        Pivot = transform.parent;
+    }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.GetComponent<Rigidbody2D>() != null && transform.localEulerAngles.z != 0) {
+        if (other.gameObject.GetComponent<Rigidbody2D>() != null && Pivot.localEulerAngles.z != 0) {
             Debug.Log("Collision");
             other.gameObject.GetComponent<Rigidbody2D>().AddForce(other.GetContact(0).normal.normalized * Force * -1f, ForceMode2D.Impulse); //Rigidbody _rb = 
             //StartCoroutine(HandleCollisions(other.collider));
