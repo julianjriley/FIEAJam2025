5e9768de52238ae3662202eb9f45c61a902f2210
Jane Nicole Apostol <89412671+jnapostol@users.noreply.github.com>
2025-01-19 09:39:46 -0500

    audio and menus

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -6,19 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public AudioClip clip;
+    private AudioSource _source;
     //public string NextSceneName;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _source = GetComponent<AudioSource>();
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (SceneManager.GetActiveScene().name == "Credits")
+        {
+            _source.clip = clip;
+        }
     }
-
 
     public void Quit()
     {
