159503eef2409aba1fe6f035831f3327eac952f2
Jane Nicole Apostol <89412671+jnapostol@users.noreply.github.com>
2025-01-31 01:03:12 -0500

    um yea im not good at timers but i try

--- Assets/Scripts/Stage/StageTimer.cs
+++ Assets/Scripts/Stage/StageTimer.cs
@@ -9,45 +9,60 @@ public class StageTimer : MonoBehaviour
 {
     public static event Action <StageTimer> OnTimerStart;
     public static event Action OnTimerEnd;
+    public TextMeshProUGUI CountdownText;
     public TextMeshProUGUI TimerText;
     private Animator _textAnim;
     [SerializeField]
     private float TotalTime = 60;
     [SerializeField]
     private GameObject _transitionObj;
+    private bool _hasStarted;
 
     void Start()
     {
         TimerText.text = TotalTime.ToString();
+        _hasStarted = false;
+        StartCoroutine(Countdown());
+        // Startcoroutine to countdown set a bool hasStarted = false. end of coroutine hasStarted = true;
     }
 
+    IEnumerator Countdown()
+    {
+        
+        yield return new WaitForSeconds(3f);
+        _hasStarted = true;
+    }
     public float GetTime() {
         return TotalTime;
     }
 
     void Update()
     {
-        if (TotalTime == 10)
+        if (_hasStarted == true)
         {
-            // get rid of the crown haver
-            // TODO: Make TextChange animation in Animator 
-            _textAnim.Play("TextChange");
-        }
-        if (TotalTime > 0)
-        {
-            TotalTime -= Time.deltaTime;
-            float sec = Mathf.RoundToInt(TotalTime);
-            TimerText.text = sec.ToString();
-        }
-        else
-        {
-            TotalTime = 0;
-            TimerText.text = TotalTime.ToString();
+            if (TotalTime == 10)
+            {
+                // get rid of the crown haver
+                // TODO: Make TextChange animation in Animator 
+                _textAnim.Play("TextChange");
+            }
+            if (TotalTime > 0)
+            {
+                TotalTime -= Time.deltaTime;
+                float sec = Mathf.RoundToInt(TotalTime);
+                TimerText.text = sec.ToString();
+            }
+            else
+            {
+                TotalTime = 0;
+                TimerText.text = TotalTime.ToString();
 
-            StartCoroutine(StartTransitionSequence());
-            // play sound
-            // connect to leaderboard
+                StartCoroutine(StartTransitionSequence());
+                // play sound
+                // connect to leaderboard
+            }
         }
+        
     }
 
     IEnumerator StartTransitionSequence()
